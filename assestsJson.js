71e64da991cd8abab31111d650c387b7a0e4a830
irohitb <irohitbhatia@gmail.com>
2021-09-19 10:42:10 +0530

    feat: script to auto create imports and images

--- assestsJson.js
+++ assestsJson.js
@@ -1,12 +1,14 @@
+//Note don't change indentation of this file
+// we use string literals when creating ts file, this is done to make syntax appera on next line
+
 //requiring path and fs modules
 const path = require('path');
 const fs = require('fs');
 //joining path of directory 
 
 const directoryPath = path.join(__dirname, '/src/assests/images/avatars');
-
-const imageData = []
-
+const imagesData = []
+let codeStr = ''
 //passsing directoryPath and callback function
 fs.readdir(directoryPath, function (err, files) {
     //handling error
@@ -15,14 +17,52 @@ fs.readdir(directoryPath, function (err, files) {
     } 
     //listing all files using forEach
     files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        if (file.indexOf("@") == -1 && file !== ".DS_Store") {
-            imageData.push(file)
+        if (file !== ".DS_Store") {
+            let imageImportPath = file.split(".")[0].split(' ').join('')
+            let name = file.split(' ')[0]
+            if (file.indexOf("@") != -1 ) {
+                name = file.split('@')[0]
+                imageImportPath = name.split(' ').join('') + file.split('@')[1].split('.')[0]
+            }
+
+            codeStr = codeStr + `import ${imageImportPath} from "images/avatars/${file}";
+`             
+            if (file.indexOf("@") == -1) {
+                imagesData.push({
+                    "name": name,
+                    "path": imageImportPath,
+                    "path2x": `${imageImportPath}2x`,
+                    "path3x": `${imageImportPath}3x`
+                })
+            } 
         }
     });
-    fs.writeFileSync('./src/constants/images/index.json', JSON.stringify(
-       [...imageData]
-    , null, 4))
-});
+    codeStr = codeStr + `
+
+const imagesData = [`
+
+imagesData.forEach((el, index) => {
+    codeStr = codeStr + `
+ {
+    name: "${el.name}",
+    path: ${el.path}, 
+    path2x: ${el.path2x},
+    path3x: ${el.path3x}
+ }`
+if (index !== imagesData.length  -1) codeStr = codeStr + ','
+})
+
+codeStr = codeStr + `
+]
+
+export default imagesData
+` 
+fs.writeFile('./src/assests/images/index.ts', codeStr, 'utf8', function(err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("The file was saved!");
+    });
+})
 
 
---
+++ src/assests/images/index.ts
@@ -0,0 +1,257 @@
+import AllanMunger from "images/avatars/Allan Munger.png"
+import AllanMunger2x from "images/avatars/Allan Munger@2x.png"
+import AllanMunger3x from "images/avatars/Allan Munger@3x.png"
+import AmandaBrady from "images/avatars/Amanda Brady.png"
+import AmandaBrady2x from "images/avatars/Amanda Brady@2x.png"
+import AmandaBrady3x from "images/avatars/Amanda Brady@3x.png"
+import AshleyMcCarthy from "images/avatars/Ashley Mc Carthy.png"
+import AshleyMcCarthy2x from "images/avatars/Ashley Mc Carthy@2x.png"
+import AshleyMcCarthy3x from "images/avatars/Ashley Mc Carthy@3x.png"
+import CarlosSlattery from "images/avatars/Carlos Slattery.png"
+import CarlosSlattery2x from "images/avatars/Carlos Slattery@2x.png"
+import CarlosSlattery3x from "images/avatars/Carlos Slattery@3x.png"
+import CarolePoland from "images/avatars/Carole Poland.png"
+import CarolePoland2x from "images/avatars/Carole Poland@2x.png"
+import CarolePoland3x from "images/avatars/Carole Poland@3x.png"
+import CecilFolk from "images/avatars/Cecil Folk.png"
+import CecilFolk2x from "images/avatars/Cecil Folk@2x.png"
+import CecilFolk3x from "images/avatars/Cecil Folk@3x.png"
+import CelesteBurton from "images/avatars/Celeste Burton.png"
+import CelesteBurton2x from "images/avatars/Celeste Burton@2x.png"
+import CelesteBurton3x from "images/avatars/Celeste Burton@3x.png"
+import CharlotteWaltson from "images/avatars/Charlotte Waltson.png"
+import CharlotteWaltson2x from "images/avatars/Charlotte Waltson@2x.png"
+import CharlotteWaltson3x from "images/avatars/Charlotte Waltson@3x.png"
+import ColinBallinger from "images/avatars/Colin Ballinger.png"
+import ColinBallinger2x from "images/avatars/Colin Ballinger@2x.png"
+import ColinBallinger3x from "images/avatars/Colin Ballinger@3x.png"
+import DaisyPhillips from "images/avatars/Daisy Phillips.png"
+import DaisyPhillips2x from "images/avatars/Daisy Phillips@2x.png"
+import DaisyPhillips3x from "images/avatars/Daisy Phillips@3x.png"
+import ElliotWoodward from "images/avatars/Elliot Woodward.png"
+import ElliotWoodward2x from "images/avatars/Elliot Woodward@2x.png"
+import ElliotWoodward3x from "images/avatars/Elliot Woodward@3x.png"
+import ElviaAtkins from "images/avatars/Elvia Atkins.png"
+import ElviaAtkins2x from "images/avatars/Elvia Atkins@2x.png"
+import ElviaAtkins3x from "images/avatars/Elvia Atkins@3x.png"
+import ErikNason from "images/avatars/Erik Nason.png"
+import ErikNason2x from "images/avatars/Erik Nason@2x.png"
+import ErikNason3x from "images/avatars/Erik Nason@3x.png"
+import HenryBrill from "images/avatars/Henry Brill.png"
+import HenryBrill2x from "images/avatars/Henry Brill@2x.png"
+import HenryBrill3x from "images/avatars/Henry Brill@3x.png"
+import IsaacFielder from "images/avatars/Isaac Fielder.png"
+import IsaacFielder2x from "images/avatars/Isaac Fielder@2x.png"
+import IsaacFielder3x from "images/avatars/Isaac Fielder@3x.png"
+import JohnieMcConnell from "images/avatars/Johnie McConnell.png"
+import JohnieMcConnell2x from "images/avatars/Johnie McConnell@2x.png"
+import JohnieMcConnell3x from "images/avatars/Johnie McConnell@3x.png"
+import Katlarsson from "images/avatars/Kat larsson.png"
+import Katlarsson2x from "images/avatars/Kat larsson@2x.png"
+import Katlarsson3x from "images/avatars/Kat larsson@3x.png"
+import KatriAhokas from "images/avatars/Katri Ahokas.png"
+import KatriAhokas2x from "images/avatars/Katri Ahokas@2x.png"
+import KatriAhokas3x from "images/avatars/Katri Ahokas@3x.png"
+import KevinSturgis from "images/avatars/Kevin Sturgis.png"
+import KevinSturgis2x from "images/avatars/Kevin Sturgis@2x.png"
+import KevinSturgis3x from "images/avatars/Kevin Sturgis@3x.png"
+import KristinPatterson from "images/avatars/Kristin Patterson.png"
+import KristinPatterson2x from "images/avatars/Kristin Patterson@2x.png"
+import KristinPatterson3x from "images/avatars/Kristin Patterson@3x.png"
+import LydiaBauer from "images/avatars/Lydia Bauer.png"
+import LydiaBauer2x from "images/avatars/Lydia Bauer@2x.png"
+import LydiaBauer3x from "images/avatars/Lydia Bauer@3x.png"
+import MauricioAugust from "images/avatars/Mauricio August.png"
+import MauricioAugust2x from "images/avatars/Mauricio August@2x.png"
+import MauricioAugust3x from "images/avatars/Mauricio August@3x.png"
+import MiguelGarcia from "images/avatars/Miguel Garcia.png"
+import MiguelGarcia2x from "images/avatars/Miguel Garcia@2x.png"
+import MiguelGarcia3x from "images/avatars/Miguel Garcia@3x.png"
+import MonaKane from "images/avatars/Mona Kane.png"
+import MonaKane2x from "images/avatars/Mona Kane@2x.png"
+import MonaKane3x from "images/avatars/Mona Kane@3x.png"
+import RobertTolbert from "images/avatars/Robert Tolbert.png"
+import RobertTolbert2x from "images/avatars/Robert Tolbert@2x.png"
+import RobertTolbert3x from "images/avatars/Robert Tolbert@3x.png"
+import RobinCounts from "images/avatars/Robin Counts.png"
+import RobinCounts2x from "images/avatars/Robin Counts@2x.png"
+import RobinCounts3x from "images/avatars/Robin Counts@3x.png"
+import TimDeboer from "images/avatars/Tim Deboer.png"
+import TimDeboer2x from "images/avatars/Tim Deboer@2x.png"
+import TimDeboer3x from "images/avatars/Tim Deboer@3x.png"
+import WandaHoward from "images/avatars/Wanda Howard.png"
+import WandaHoward2x from "images/avatars/Wanda Howard@2x.png"
+import WandaHoward3x from "images/avatars/Wanda Howard@3x.png"
+
+const imagesData = [
+  {
+    name: "Allan",
+    path: AllanMunger,
+    path2x: AllanMunger2x,
+    path3x: AllanMunger3x
+  },
+  {
+    name: "Amanda",
+    path: AmandaBrady,
+    path2x: AmandaBrady2x,
+    path3x: AmandaBrady3x
+  },
+  {
+    name: "Ashley",
+    path: AshleyMcCarthy,
+    path2x: AshleyMcCarthy2x,
+    path3x: AshleyMcCarthy3x
+  },
+  {
+    name: "Carlos",
+    path: CarlosSlattery,
+    path2x: CarlosSlattery2x,
+    path3x: CarlosSlattery3x
+  },
+  {
+    name: "Carole",
+    path: CarolePoland,
+    path2x: CarolePoland2x,
+    path3x: CarolePoland3x
+  },
+  {
+    name: "Cecil",
+    path: CecilFolk,
+    path2x: CecilFolk2x,
+    path3x: CecilFolk3x
+  },
+  {
+    name: "Celeste",
+    path: CelesteBurton,
+    path2x: CelesteBurton2x,
+    path3x: CelesteBurton3x
+  },
+  {
+    name: "Charlotte",
+    path: CharlotteWaltson,
+    path2x: CharlotteWaltson2x,
+    path3x: CharlotteWaltson3x
+  },
+  {
+    name: "Colin",
+    path: ColinBallinger,
+    path2x: ColinBallinger2x,
+    path3x: ColinBallinger3x
+  },
+  {
+    name: "Daisy",
+    path: DaisyPhillips,
+    path2x: DaisyPhillips2x,
+    path3x: DaisyPhillips3x
+  },
+  {
+    name: "Elliot",
+    path: ElliotWoodward,
+    path2x: ElliotWoodward2x,
+    path3x: ElliotWoodward3x
+  },
+  {
+    name: "Elvia",
+    path: ElviaAtkins,
+    path2x: ElviaAtkins2x,
+    path3x: ElviaAtkins3x
+  },
+  {
+    name: "Erik",
+    path: ErikNason,
+    path2x: ErikNason2x,
+    path3x: ErikNason3x
+  },
+  {
+    name: "Henry",
+    path: HenryBrill,
+    path2x: HenryBrill2x,
+    path3x: HenryBrill3x
+  },
+  {
+    name: "Isaac",
+    path: IsaacFielder,
+    path2x: IsaacFielder2x,
+    path3x: IsaacFielder3x
+  },
+  {
+    name: "Johnie",
+    path: JohnieMcConnell,
+    path2x: JohnieMcConnell2x,
+    path3x: JohnieMcConnell3x
+  },
+  {
+    name: "Kat",
+    path: Katlarsson,
+    path2x: Katlarsson2x,
+    path3x: Katlarsson3x
+  },
+  {
+    name: "Katri",
+    path: KatriAhokas,
+    path2x: KatriAhokas2x,
+    path3x: KatriAhokas3x
+  },
+  {
+    name: "Kevin",
+    path: KevinSturgis,
+    path2x: KevinSturgis2x,
+    path3x: KevinSturgis3x
+  },
+  {
+    name: "Kristin",
+    path: KristinPatterson,
+    path2x: KristinPatterson2x,
+    path3x: KristinPatterson3x
+  },
+  {
+    name: "Lydia",
+    path: LydiaBauer,
+    path2x: LydiaBauer2x,
+    path3x: LydiaBauer3x
+  },
+  {
+    name: "Mauricio",
+    path: MauricioAugust,
+    path2x: MauricioAugust2x,
+    path3x: MauricioAugust3x
+  },
+  {
+    name: "Miguel",
+    path: MiguelGarcia,
+    path2x: MiguelGarcia2x,
+    path3x: MiguelGarcia3x
+  },
+  {
+    name: "Mona",
+    path: MonaKane,
+    path2x: MonaKane2x,
+    path3x: MonaKane3x
+  },
+  {
+    name: "Robert",
+    path: RobertTolbert,
+    path2x: RobertTolbert2x,
+    path3x: RobertTolbert3x
+  },
+  {
+    name: "Robin",
+    path: RobinCounts,
+    path2x: RobinCounts2x,
+    path3x: RobinCounts3x
+  },
+  {
+    name: "Tim",
+    path: TimDeboer,
+    path2x: TimDeboer2x,
+    path3x: TimDeboer3x
+  },
+  {
+    name: "Wanda",
+    path: WandaHoward,
+    path2x: WandaHoward2x,
+    path3x: WandaHoward3x
+  }
+]
+
+export default imagesData
